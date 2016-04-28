819c39b6f0d2739ae2290f8291dfdab02a2eb659
Konstantin <kostya@mazur.in>
2016-04-28 04:32:06 +0300

    added 7 report

--- Time_rows/#5.py
+++ Time_rows/#5.py
@@ -5,11 +5,16 @@ import matplotlib.pyplot as plt
 
 N = 460
 d_t = 1
-nu1 = 0.1
+nu1 = 0.2
+nu2 = 0.15
+A1 = 1
+A2 = 1
+fi1 = 0
+fi2 = 0
 x = []
 
 for k in range(N/2):
-	x.extend([math.cos(2 * math.pi * nu1 * d_t * k)] * 2)
+	x.extend([A1 * math.cos(2 * math.pi * nu1 * d_t * k - fi1) + A2 * math.cos(2 * math.pi * nu2 * d_t * k - fi2)] * 2)
 
 p = math.ceil(math.log(N,2))
 N2 = int(2**(p+1))
